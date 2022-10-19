9f9832fd263a457b411ce3f35c4ac5494094d160
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-19 12:29:07 +0600

    code uploaded

---
+++ src/ForLoopDemo3/ForLoopDemo3.java
@@ -0,0 +1,28 @@
+
+package ForLoopDemo3;
+
+/**
+ *
+ * @author nayeem
+ */
+public class ForLoopDemo3 {
+    public static void main(String[]args)
+            
+    {
+     int sum=0;
+     
+     for(int x=1;x<=10;x++)
+     {
+     
+     sum=sum+x;
+     
+     }
+     
+     
+     System.out.println("The sum of 1 to 10 number by using for loop is:"+sum);
+    
+    
+    
+    }
+    
+}
